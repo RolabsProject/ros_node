dc56c76e084b7682cd0883a647f8d8cb5e66d739
thelme <anthelme@hotmail.fr>
2014-05-24 00:54:53 +0000

    utilisation du multi array float 32  pour les capteurs ! Ho yeah

--- adcpi/scripts/adcpi_node.py
+++ adcpi/scripts/adcpi_node.py
@@ -3,31 +3,31 @@
 
 import rospy
 from adcpi import adcpi
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 
 
 def adcpi_node():
-    global capteurs
+    global adcpiCap
 
+    pub = rospy.Publisher('capteurs', Float32MultiArray )
    
-    pub_capteurs = []
-    for i in range(0, 4+1):
-        pub_capteurs.append( rospy.Publisher('capteurs/capteur%d'%i, Float32) )
- 
-
     rospy.init_node('adcpi_node', anonymous=True)
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
 
+        val_capteurs = []
 	for i in range(0, 4+1):
-        	pub_capteurs[i].publish( capteurs.capteurRead(i) )
-	
-        #rospy.loginfo("Capteurs: " + str(val_capteurs))
+        	val_capteurs.append( adcpiCap.capteurRead(i) )
+
+        capteurs = Float32MultiArray()
+        capteurs.data = val_capteurs
+        pub.publish(capteurs)
+        
         r.sleep()
         
 if __name__ == '__main__':
-    global capteurs
-    capteurs = adcpi()
+    global adcpiCap
+    adcpiCap = adcpi()
     try:
         adcpi_node()
     except rospy.ROSInterruptException: pass
