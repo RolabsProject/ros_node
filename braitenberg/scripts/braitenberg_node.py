12bf3cb2e7d8416ed60eead951f080f2f619980d
thelme <anthelme@hotmail.fr>
2014-05-24 01:44:54 +0000

    évo de brain

--- braitenberg/scripts/braitenberg_node.py
+++ braitenberg/scripts/braitenberg_node.py
@@ -2,47 +2,48 @@
 
 import rospy
 from std_msgs.msg import String
-from std_msgs.msg import Float32
-#from cmdMoteur.msg import Cmd
+from std_msgs.msg import Float32MultiArray, ByteMultiArray
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id()+"I heard %f",data.data)
+   # print data
+
+    Vx    = 0
+    Vy    = 0
+    theta = 0
+
+
+    Vy = 64 -32*int(data.data[2]) -16*int(data.data[1]) -16*int(data.data[3]) -8*int(data.data[0]) -8*int(data.data[4])
+
+
+    #Cast des valeurs
+    if Vx > 127:
+        Vx = 127
+    elif Vx < (-127):
+        Vx = -127
+    if Vy > 127:
+        Vy = 127
+    elif Vy <(-127):
+        Vy = -127
+	
+    cmdstr = str(Vx) + "_" + str(Vy) + "_" + str(theta)
+    rospy.loginfo(cmdstr)
+    pub.publish(cmdstr)
+
+
+
     
 def braitenberg():
 
     rospy.init_node('braitenberg', anonymous=True)
-
-    for i in range(0, 4+1):
-    	rospy.Subscriber("capteurs/capteur%d"%i, Float32, callback)
-
-    #rospy.Subscriber("capteurs/capteur0", Float32, callback)
+    rospy.Subscriber('capteurs', Float32MultiArray, callback)
     rospy.spin()
-
 
     pub = rospy.Publisher('cmd', String) #, queue_size=10)
     r = rospy.Rate(10) # 10hz
    
 
     while not rospy.is_shutdown():
-        Vx    = 0
-        Vy    = 0
-        theta = 0
-
-
-	#Cast des valeurs
-	if Vx > 127:
-	    Vx = 127
-	elif Vx < (-127):
-	    Vx = -127
-	if Vy > 127:
-	    Vy = 127
-	elif Vy <(-127):
-	    Vy = -127
-	
-        str = Vx+"_"+Vy+"_"+theta
-        rospy.loginfo(str)
-        pub.publish(str)
         r.sleep()
         
 if __name__ == '__main__':
