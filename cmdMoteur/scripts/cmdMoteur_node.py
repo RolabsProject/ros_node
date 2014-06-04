40005a69f635a13f6a5465f26d906af73293cf91
Kalash78 <prounyjulien@gmail.com>
2014-06-04 10:01:18 +0200

    Update cmdMoteur_node.py

--- cmdMoteur/scripts/cmdMoteur_node.py
+++ cmdMoteur/scripts/cmdMoteur_node.py
@@ -3,24 +3,23 @@
 import rospy
 from cmdMoteur import cmdMoteur
 from std_msgs.msg import String
-from cmdMoteur.msg import Cmd
 
 def callback(data):
     global myCmdMoteur
 
     #rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
     cmd = data.data.split(' ')
-    msg = Cmd(vx=int(cmd[0]),
-              vy=int(cmd[1]),
-              theta=int(cmd[2]))
-    rospy.loginfo("Vx : %d\tVy : %d\tTheta : %d "%(msg.vx, msg.vy, msg.theta) )
-    myCmdMoteur.envoiCommande(msg)
+    Vx = int(cmd[0])
+    Vy = int(cmd[1])
+    theta = int(cmd[2])
+    rospy.loginfo("Vx : %d\tVy : %d\tTheta : %d "%(Vx, Vy, theta) )
+    myCmdMoteur.envoiCommande(Vx,Vy,theta)
 
 def cmdMoteur_node():
     global myCmdMoteur
     myCmdMoteur = cmdMoteur()
     rospy.init_node('cmdMoteur_node', anonymous=True)
-    rospy.Subscriber("cmd", Cmd, callback)
+    rospy.Subscriber("cmd", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
