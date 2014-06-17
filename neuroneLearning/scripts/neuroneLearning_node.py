4204ced448eaf323e806e396da8e5079f40de4f0
thelme <anthelme@hotmail.fr>
2014-06-17 09:44:21 +0200

    Descente de gradiant fonctionnel avec des exemples automatiques

--- neuroneLearning/scripts/neuroneLearning_node.py
+++ neuroneLearning/scripts/neuroneLearning_node.py
@@ -21,47 +21,56 @@ def callback(data):
     global errStaticiTheta
 
     global coefVy
+    global coefVx
     global coefTheta 
 
     global d_coefVy
+    global d_coefVx
     global d_coefTheta 
 
     #"""""""""""""""""""""""""""""""""""""""
-    alpha = 1
+    alpha = 0.01
 
     #"""""""""""""""""""""""""""""""""""""""
-    xk       = data.data[0:5]
-    yk_vx    = data.data[5]
-    yk_vy    = data.data[6]
+    xk       = data.data[0:5] #valeurs capteurs par exemple
+    yk_vx    = data.data[5]   #valeurs Vitesse x par exemple
+    yk_vy    = data.data[6]   #valeurs Vitesse y par exemple
     yk_theta = data.data[7]
 
     #"""""""""""""""""""""""""""""""""""""""
     sk = calculeOut(xk)
 
     #"""""""""""""""""""""""""""""""""""""""
-    #print "Xk : ", xk, "\t Vy : ", yk_vy, "\t Theta : ", yk_theta, "\t Sk : ", sk   
+    #print "Xk : ", xk, "\t Vy : ", yk_vy, "\t Vx : ", yk_vx, "\t Theta : ", yk_theta, 
  
     #"""""""""""""""""""""""""""""""""""""""
     for i in range(0, 5): #5 capteurs
-        d_coefVy[i]    =  d_coefVy[i]    + alpha*(yk_vy    - sk['Vy']    )*xk[i]
-        d_coefTheta[i] =  d_coefTheta[i] + alpha*(yk_theta - sk['Theta'] )*xk[i]
+        d_coefVy[i]    =  d_coefVy[i]    + alpha*(yk_vy    - sk['Vy']    ) *xk[i]
+        d_coefVx[i]    =  d_coefVx[i]    + alpha*(yk_vx    - sk['Vx']    ) *xk[i]
+        d_coefTheta[i] =  d_coefTheta[i] + alpha*(yk_theta - sk['Theta'] ) *xk[i]
 
-    print "coefVy : ", coefVy + d_coefVy, "\tcoefTheta : ", coefTheta + d_coefTheta
+    print "\tcoefVy : ", coefVy + d_coefVy, "\tcoefVx : ", coefVx + d_coefVx, "\tcoefTheta : ", coefTheta + d_coefTheta
     i+=1
 
 ###################################################################################
 def calculeOut(array_capteurs):
     #"""""""""""""""""""""""""""""""""""""""
     global coefVy
+    global coefVx
     global coefTheta 
 
+    global d_coefVy
+    global d_coefVx
+    global d_coefTheta
+
     #"""""""""""""""""""""""""""""""""""""""
-    vy    =  coefVy    *  array_capteurs
-    theta =  coefTheta *  array_capteurs
+    vy    =  (coefVy+d_coefVy)	     *  array_capteurs
+    vx    =  (coefVx+d_coefVx)	     *  array_capteurs
+    theta =  (coefTheta+d_coefTheta) *  array_capteurs
 
     #"""""""""""""""""""""""""""""""""""""""
     Vy    = 64 + vy.sum()
-    Vx    = 0
+    Vx    = 0  + vx.sum()
     theta = 0  + theta.sum()
 
     #"""""""""""""""""""""""""""""""""""""""
@@ -92,9 +101,8 @@ def neuroneLearning():
        r.sleep()
 
     
-    for i in range(0, 5): #5 capteurs
-        coefVy[i]    = coefVy[i]    + d_coefVy[i]
-        coefTheta[i] = coefTheta[i] + d_coefVy[i]
+    coefVy    = coefVy    + d_coefVy
+    coefTheta = coefTheta + d_coefTheta
 
     print "coefVy : ", coefVy
     print "coefTheta : ", coefTheta
@@ -106,9 +114,11 @@ if __name__ == '__main__':
     #"""""""""""""""""""""""""""""""""""""""
     global i 
     global coefVy
+    global coefVx
     global coefTheta 
 
     global d_coefVy
+    global d_coefVx
     global d_coefTheta 
 
     global errStaticiVx 
@@ -116,11 +126,16 @@ if __name__ == '__main__':
     global errStaticiTheta
 
     #"""""""""""""""""""""""""""""""""""""""
-    coefVy    = array([-8, -20, -32, -20, -8])
-    coefTheta = array([-1, -5, +2, +5, +1])
-
-    d_coefVy    = array([0, 0, 0, 0, 0])
-    d_coefTheta = array([0, 0, 0, 0, 0])
+    #coefVy    = array([-8, -20, -32, -20,  -8])
+    #coefTheta = array([-1,  -5,  +2,  +5,  +1])
+    #coefVy    = array([-1, -10, -23, -10,  -1])
+    coefVx    = array([ 0,   0,   0,   0,   0])
+    coefVy    = array([ 0,   0,   0,   0,   0])
+    #coefTheta = array([-0,  -1,  +1,  +2,  +0])
+    coefTheta    = array([ 0,   0,   0,  0,   0])
+    d_coefVy    = array([0., 0., 0., 0., 0.])
+    d_coefVx    = array([0., 0., 0., 0., 0.])
+    d_coefTheta = array([0., 0., 0., 0., 0.])
 
     errStaticiVx = 0
     errStaticiVy = 0
